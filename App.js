17bb4dfe50f7fed107e3712f4761b864baec2bab
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2020-11-16 13:46:55 -0600

    Basic navigation and style

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import { Container, Content, Text, StyleProvider, Left, Title, Button, Header, List, ListItem, Separator, Icon, Body, View, Fab, Right } from 'native-base';
+import { Container, Content, Text, StyleProvider, H1, H2, Left, Title, Button, Header, List, ListItem, Separator, Icon, Body, View, Fab, Right, Tab, Tabs, ScrollableTab } from 'native-base';
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material';
 import AsyncStorage from '@react-native-community/async-storage';
 import { Alert } from 'react-native';
-export default class ThemeExample extends Component {
+import * as Font from 'expo-font';
+import MainTab from './MainTab';
+import Add from './Add';
+import { createStackNavigator } from '@react-navigation/stack';
+import {
+  NavigationContainer
+} from '@react-navigation/native';
 
+class MainScreen extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      active: false
+      data: [],
+      loading: true
     }
+    this.storeData = this.storeData.bind(this);
+    this.getData = this.getData.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleEditItem = this.handleEditItem.bind(this)
   }
 
-  storeData = async (key, value) => {
+  async storeData(key, value) {
     try {
       await AsyncStorage.setItem(key, value)
     } catch (e) { // saving error
     }
   }
 
-  getData = async (key) => {
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    })
+    this.setState({ loading: false })
+  }
+
+  async getData(key) {
     try {
       const value = await AsyncStorage.getItem(key)
       if (value !== null) { // value previously stored
@@ -30,115 +50,95 @@ export default class ThemeExample extends Component {
     }
   }
 
+  async handleAddItem(obj) {
+
+  }
+
+  async handleEditItem(obj) {
+
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <View></View>
+      );
+    }
     return (
-      <StyleProvider style={getTheme(material)}>
-        <Container>
-          <Header >
-            <Left />
-            <Body>
-              <Title>Organize</Title>
-            </Body>
-            <Right />
-          </Header>
-          <Content>
-            <Text>
-              I have changed the text color.
-            </Text>
-            <Button light><Text> Light </Text></Button>
-            <Button primary><Text> Primary </Text></Button>
-            <Button success><Text> Success </Text></Button>
 
-            <List>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem button onPress={() => { Alert.alert("aaa"); }}>
-                <Body>
-                  <Text>Kumar Pratik</Text>
-                  <Text note>Doing what you like will always keep you happy . .</Text>
-                </Body>
-                <Right><Icon name="arrow-forward" /></Right>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem>
-                <Text>Caroline Aaron</Text>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem>
-                <Body>
-                  <Text>Kumar Pratik</Text>
-                  <Text note>Doing what you like will always keep you happy . .</Text>
-                </Body>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem>
-                <Text>Caroline Aaron</Text>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem>
-                <Body>
-                  <Text>Kumar Pratik</Text>
-                  <Text note>Doing what you like will always keep you happy . .</Text>
-                </Body>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-              <Separator bordered>
-                <Text>MIDFIELD</Text>
-              </Separator>
-              <ListItem>
-                <Text>Caroline Aaron</Text>
-              </ListItem>
-              <ListItem last>
-                <Text>Lee Allen</Text>
-              </ListItem>
-            </List>
+      <Stack.Navigator initialRouteName="Main"  screenOptions={{ 
+        headerStyle: {
+          backgroundColor: '#2b82c9',
+        },
+        headerTintColor: '#fff' }}>
+        <Stack.Screen name="Organize" options={{ header: (props) => <CustomHeader {...props} />}}>
+          {(props) =>
+            <StyleProvider style={getTheme(material)}>
+              <Container>
+                <Tabs renderTabBar={() => <ScrollableTab style={{height: '6%'}} />}>
+                  <Tab heading="MainTab"  tabStyle={{ backgroundColor: "#2b82c9" }}  activeTabStyle={{ backgroundColor: "#2b82c9" }}>
+                    <MainTab {...props} />
+                  </Tab>
+                  <Tab heading="MainTab" tabStyle={{ backgroundColor: "#2b82c9" }}  activeTabStyle={{ backgroundColor: "#2b82c9" }}>
+                    <MainTab {...props} />
+                  </Tab>
+                  <Tab heading="MainTab" tabStyle={{ backgroundColor: "#2b82c9" }}  activeTabStyle={{ backgroundColor: "#2b82c9" }}>
+                    <MainTab {...props} />
+                  </Tab>
+                  <Tab heading="MainTab" tabStyle={{ backgroundColor: "#2b82c9" }}  activeTabStyle={{ backgroundColor: "#2b82c9" }}>
+                    <MainTab {...props} />
+                  </Tab>
+                  <Tab heading="MainTab" tabStyle={{ backgroundColor: "#2b82c9" }}  activeTabStyle={{ backgroundColor: "#2b82c9" }}>
+                    <MainTab {...props} />
+                  </Tab>
+                </Tabs>
+              </Container>
+            </StyleProvider>}
+        </Stack.Screen>
+        <Stack.Screen name="Add" options={{ header: (props) => <CustomHeader {...props} />}}>
+          {(props) => <Add {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
 
 
-          </Content>
 
-          <Fab
-            active={this.state.active}
-            direction="up"
-            containerStyle={{}}
-            style={{ backgroundColor: '#5067FF' }}
-            position="bottomRight"
-            onPress={() => this.setState({ active: !this.state.active })}>
-            <Icon name="share" />
-            <Button style={{ backgroundColor: '#34A34F' }}>
-              <Icon name="logo-whatsapp" />
-            </Button>
-            <Button style={{ backgroundColor: '#3B5998' }}>
-              <Icon name="logo-facebook" />
-            </Button>
-            <Button disabled style={{ backgroundColor: '#DD5144' }}>
-              <Icon name="mail" />
-            </Button>
-          </Fab>
-        </Container>
-      </StyleProvider>
     );
   }
+
+}
+
+const CustomHeader = ({scene, previous, navigation}) => {
+  const {options} = scene.descriptor;
+  const title =
+    options.headerTitle !== undefined
+      ? options.headerTitle
+      : options.title !== undefined
+      ? options.title
+      : scene.route.name;
+
+  return (
+    <Header>
+      {previous ? (
+          <Left><Button transparent onPress={navigation.goBack}>
+            <Icon name="arrow-back" />
+          </Button></Left>
+        ) : <View style={{paddingLeft: 10}}></View>}
+      
+      <Body><Title>{title}</Title></Body>
+      <Right></Right>
+    </Header>
+  );
+};
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <MainScreen />
+
+    </NavigationContainer>
+  );
 }
---
+++ MainTab.js
@@ -0,0 +1,200 @@
+import React, { Component } from 'react';
+import { Container, Content, Text, StyleProvider, Left, Title, Button, Header, List, ListItem, Separator, Icon, Body, View, Fab, Right } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Alert } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+export default class MainScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            date: new Date()
+        }
+        this.storeData = this.storeData.bind(this);
+        this.getData = this.getData.bind(this);
+        this.setDate = this.setDate.bind(this);
+        this.setMode = this.setMode.bind(this);
+        this.setshow = this.setShow.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    setDate(dat) {
+        this.setState({ date: dat });
+    }
+
+    setMode(variable) {
+        this.setState({ mode: variable });
+    }
+
+    setShow(bool) {
+        this.setState({ show: bool });
+    }
+
+    onChange(event, selectedDate) {
+        const currentDate = selectedDate || this.state.date;
+        this.setShow(Platform.OS === 'ios');
+        this.setDate(currentDate);
+    }
+
+    showMode = (currentMode) => {
+        this.setShow(true);
+        this.setMode(currentMode);
+    };
+
+    showDatepicker = () => {
+        this.showMode('date');
+    };
+
+    showTimepicker = () => {
+        this.showMode('time');
+    };
+
+    async storeData(key, value) {
+        try {
+            await AsyncStorage.setItem(key, value)
+        } catch (e) { // saving error
+        }
+    }
+
+    async getData(key) {
+        try {
+            const value = await AsyncStorage.getItem(key)
+            if (value !== null) { // value previously stored
+            }
+        } catch (e) { // error reading value
+        }
+    }
+
+    async handleAddItem(obj) {
+
+    }
+
+    async handleEditItem(obj) {
+
+    }
+
+    render() {
+        return (
+
+            <StyleProvider style={getTheme(material)}>
+                <Container>
+                    <Content>
+                        <Text>
+                            I have changed the text color.
+            </Text>
+                        <Button light><Text> Light </Text></Button>
+                        <Button primary><Text> Primary </Text></Button>
+                        <Button success><Text> Success </Text></Button>
+
+                        <Button primary onPress={this.showDatepicker}><Text>Show date picker!</Text></Button>
+
+                        {this.state.show && (
+                            <DateTimePicker
+                                testID="dateTimePicker"
+                                value={this.state.date}
+                                mode={this.state.mode}
+                                is24Hour={true}
+                                display="default"
+                                onChange={this.onChange}
+                                style={{ width: "90%" }}
+                            />)}
+                        <Text>{this.state.date.toDateString()}</Text>
+                        <List>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem button onPress={() => { Alert.alert("aaa"); }}>
+                                <Body>
+                                    <Text>Kumar Pratik</Text>
+                                    <Text note>Doing what you like will always keep you happy . .</Text>
+                                </Body>
+                                <Right><Icon name="arrow-forward" /></Right>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem>
+                                <Text>Caroline Aaron</Text>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem>
+                                <Body>
+                                    <Text>Kumar Pratik</Text>
+                                    <Text note>Doing what you like will always keep you happy . .</Text>
+                                </Body>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem>
+                                <Text>Caroline Aaron</Text>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem>
+                                <Body>
+                                    <Text>Kumar Pratik</Text>
+                                    <Text note>Doing what you like will always keep you happy . .</Text>
+                                </Body>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                            <Separator bordered>
+                                <Text>MIDFIELD</Text>
+                            </Separator>
+                            <ListItem>
+                                <Text>Caroline Aaron</Text>
+                            </ListItem>
+                            <ListItem last>
+                                <Text>Lee Allen</Text>
+                            </ListItem>
+                        </List>
+
+
+                    </Content>
+
+                    <Fab
+                        active={this.state.active}
+                        direction="up"
+                        containerStyle={{}}
+                        style={{ backgroundColor: '#5067FF' }}
+                        position="bottomRight"
+                        onPress={() => this.setState({ active: !this.state.active })}>
+                        <Icon name="add" />
+                        <Button style={{ backgroundColor: '#34A34F' }} onPress={()=>{this.props.navigation.navigate("Add")}}>
+                            <Icon name="logo-whatsapp" />
+                        </Button>
+                        <Button style={{ backgroundColor: '#3B5998' }}>
+                            <Icon name="logo-facebook" />
+                        </Button>
+                        <Button disabled style={{ backgroundColor: '#DD5144' }}>
+                            <Icon name="mail" />
+                        </Button>
+                    </Fab>
+                </Container>
+            </StyleProvider>
+
+        );
+    }
+
+}
