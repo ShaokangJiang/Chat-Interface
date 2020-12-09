1e85599ca60da9fa2c4ce69bfe8937d03998699f
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2020-12-09 17:32:36 -0600

    fix dependency error

---
+++ Website.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View} from 'native-base';
+import { WebView } from 'react-native-webview';
+
+export default class ThemeExample extends Component {
+//data
+    render() {
+
+        //need remove some field first. like data description
+        for(let i of Object.keys(this.props.data)){
+            for(let k of this.props.data[i]){
+                delete k['Description'];
+            }
+        }
+        const runFirst = `
+          window.isNativeApp = true; 
+          localStorage.setItem("budgetingData", '`+JSON.stringify(this.props.data)+`');
+          true; // note: this is required, or you'll sometimes get silent failures
+        `;
+        console.log(runFirst);
+        return (
+            <View style={{ flex: 1 }}>
+                <WebView
+                    source={{
+                        uri:
+                            'https://capitaltwo.ga/tools/Budget/run.html',
+                    }}
+                    injectedJavaScriptBeforeContentLoaded={runFirst}
+                />
+            </View>
+        );
+    }
+}
