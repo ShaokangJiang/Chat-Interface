85a95e50a6afacc72fb10dc198dbb07ad3d44057
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2021-01-07 01:44:12 -0600

    Checkpoint

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import { GiftedChat, Send } from 'react-native-gifted-chat'
 import { AppearanceProvider, Appearance, useColorScheme } from 'react-native-appearance';
 import { Audio, Video } from 'expo-av';
 import { Alert } from 'react-native';
+import base64 from 'base-64';
+import Base64 from 'Base64';
 import * as Font from 'expo-font';
+import * as FileSystem from 'expo-file-system';
 import { createStackNavigator } from '@react-navigation/stack';
 import {
   NavigationContainer
@@ -20,26 +23,34 @@ var UniqueID = 1;
 let GlobalTheme;
 
 let count = 0;
+let finished = false;
 
 var IAM_access_token = Constants.manifest.extra.EXPO_IAM_access_token;
 var wsURI = 'wss://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/' + Constants.manifest.extra.EXPO_IAM_address + '/v1/recognize'
   + '?access_token=' + IAM_access_token
   + '&model=en-US_BroadbandModel';
-console.log(wsURI);
-var websocket = new WebSocket(wsURI);
-websocket.onopen = function (evt) {
-  websocket.send(JSON.stringify({
-    action: 'start'
-  }));
-  console.log(evt.data);
-};
-websocket.onclose = function (evt) { console.log(evt.data) };
-websocket.onmessage = function (evt) { console.log(evt.data) };
-websocket.onerror = function (evt) { console.log(evt.data) };
+//console.log(wsURI);
 
-// // // Prepare and send the audio file.
-// // websocket.send(blob);
-// websocket.send(JSON.stringify({ action: 'stop' }));
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = Base64.atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+
+  return new Blob([ia], {type:mimeString});
+}
 
 class MainScreen extends Component {
 
@@ -81,12 +92,36 @@ class MainScreen extends Component {
       });
       console.log('Starting recording..');
       const recording = new Audio.Recording();
-      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.prepareToRecordAsync({
+        android: {
+          extension: '.mp3',
+          outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+          audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_DEFAULT,
+          sampleRate: 44100,
+          numberOfChannels: 2,
+          bitRate: 128000,
+        },
+        ios: {
+          extension: '.caf',
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_MAX,
+          sampleRate: 44100,
+          numberOfChannels: 2,
+          bitRate: 128000,
+          linearPCMBitDepth: 16,
+          linearPCMIsBigEndian: false,
+          linearPCMIsFloat: false,
+        },
+      });
       await recording.startAsync();
-      recording.setOnRecordingStatusUpdate((status) => {
-        count ++;
+      recording.setOnRecordingStatusUpdate(async (status) => {
+        count++;
         console.log(status)
-        if(count > 30) this.stopRecording();
+        console.log(this.state.recording.getURI())
+        //console.log(await FileSystem.getInfoAsync(this.state.recording.getURI()))
+        if (count > 20 && !finished) {
+          this.stopRecording();
+          finished = true
+        }
       })
       this.setRecording(recording);
       console.log('Recording started');
@@ -98,12 +133,20 @@ class MainScreen extends Component {
   async stopRecording() {
     console.log('Stopping recording..');
     let recording = this.state.recording;
-    this.setRecording(undefined);
     await recording.stopAndUnloadAsync();
     const uri = recording.getURI();
     console.log('Recording stopped and stored at', uri);
-    console.log(recording)
-    
+    //console.log(recording)
+    let a = await FileSystem.readAsStringAsync(uri, {
+      encoding: FileSystem.EncodingType.Base64
+    });
+
+
+    console.log(a)
+    var base64 = "data:audio/mp3;base64,"+a
+    console.log(dataURItoBlob(base64))
+
+    //use blob to pass
   }
 
   async componentDidMount() {
@@ -115,7 +158,7 @@ class MainScreen extends Component {
     GlobalTheme = Appearance.getColorScheme();
 
     this.setState({ loading: false })
-
+    await this.startRecording();
   }
 
   addMessage(content) {
